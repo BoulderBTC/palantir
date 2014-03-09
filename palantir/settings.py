1352356611f7bce2be6e89b1645eeba27b0b677b
Erin Corson <erin.corson@colorado.edu>
2014-03-08 17:34:20 -0700

    adding location field

--- palantir/settings.py
+++ palantir/settings.py
@@ -13,11 +13,35 @@ pools_schema = {
         'type': 'string',
         'minlength': 7,
         'maxlength': 15,
+        'required': True,
+        'unique': True,
+    },
+    'coin': {
+        'type': 'string',
+        'minlength': 1,
+        'maxlength': 60,
+        'required': True,
+    },
+    'location': {
+        'type': 'dict',
+        'latitude': {
+            'type': 'number',
+            'minlength': -90,
+            'maxlength': 90,
+        },
+        'longitude': {
+            'type': 'number',
+            'minlength': -180,
+            'maxlength': 180,
+        }
     },
 }
 pools = {
     'item_title': 'pool',
-
+    'additional_lookup': {
+        'url': 'regex("(?:\d{1,3}\.){3}\d{1,3}")',
+        'field': 'ip',
+    },
     'cache_control': 'max-age=10,must-revalidate',
     'cache_expires': 10,
     'schema': pools_schema,
