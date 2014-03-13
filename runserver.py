545a69097a24da3058345744ed737603e785ea75
Erin Corson <erin.corson@colorado.edu>
2014-03-12 23:18:54 -0600

    switchign from dev server to tornado

--- runserver.py
+++ runserver.py
@@ -1,2 +1,9 @@
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
 from palantir import app
-app.run(debug=True, host='0.0.0.0', port=5005)
+app.debug = True
+http_server = HTTPServer(WSGIContainer(app))
+http_server.listen(5005)
+IOLoop.instance().start()
+#app.run(debug=True, host='0.0.0.0', port=5005)
